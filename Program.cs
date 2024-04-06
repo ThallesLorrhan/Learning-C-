2fbf8ee96a7bf8d5ae11ca61d84fa85047effad9
ThallesLorrhan <thalles.lorrhan30@gmail.com>
2024-04-06 14:34:33 -0300

    total

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello World TL");
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] atgs)
+        {
+            int number_1 = 5;
+            int number_2 = 10;
+
+            int total = (number_1 + number_2);
+
+            Console.WriteLine(total);
+        }
+    }
+}
